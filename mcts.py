b0c678f3f6093822c0af1d379b39a991147c00b5
Anna Boonyanit <annaboon@stanford.edu>
2021-11-03 22:16:35 -0700

    Get rid of b

--- mcts.py
+++ mcts.py
@@ -22,11 +22,11 @@ def main():
     for i in range(board_width):
         for j in range(board_height):
             A.append((i,j))
-    r = simulate.rollout(b,gamma,d)
+    r = simulate.rollout(b._boardHitMiss,gamma,d)
     s = b._boardHitMiss
-    MCTS(s, board_width, board_height, c=1, d=10, discount_factor=0.95, k_max=10)
+    MCTS(s, board_width, board_height, ship_sizes, c=1, d=10, discount_factor=0.95, k_max=10)
 
-def MCTS(s, board_width, board_height, c, d, discount_factor, k_max):
+def MCTS(s, board_width, board_height, ship_sizes, c, d, discount_factor, k_max):
     A = []
     for i in range(board_width):
         for j in range(board_height):
@@ -36,24 +36,24 @@ def MCTS(s, board_width, board_height, c, d, discount_factor, k_max):
     for k in range(k_max):
         # Q = np.zeros((board_width*board_height, board_width*board_height)) #why are these dimensions? 3 **, 
         # N = np.zeros((board_width*board_height, board_width*board_height))
-        simulate(d, s, A, discount_factor, c, Q, N)
+        simulate(d, s, A, discount_factor, c, Q, N, ship_sizes)
     return Q
 
 # Add function to map State (number) to the board
     
-def simulate(d, s, A, discount_factor, c, Q, N):
+def simulate(d, s, A, discount_factor, c, Q, N, ship_sizes):
     if d <= 0:
         return 0
     if (s, A[0]) not in N.keys():
         for a in A:
             N[(s, a)] = 0
             Q[(s, a)] = 0
-        return simulate.rollout(s, discount_factor, d) #can it be s? s = b._boardHitMiss
+        return simulate.rollout(s, discount_factor, d)
     # Q = np.zeros((num_states, num_actions))
     # N = np.zeros((num_states, num_actions))
     a = simulate.explore(A, N, Q, c, s)
-    s_prime, r = simulate.state_action_sim(s, a)
-    q = r + discount_factor * simulate(d - 1, s_prime, A, discount_factor, c, Q, N)
+    s_prime, r = simulate.state_action_sim(s, ship_sizes, a)
+    q = r + discount_factor * simulate(d - 1, s_prime, A, discount_factor, c, Q, N, ship_sizes)
     N[(s, a)] += 1
     Q[(s, a)] += (q - Q[(s, a)]) / N[(s, a)]
     return q
--- simulate.py
+++ simulate.py
@@ -18,11 +18,11 @@ def explore(A, N, Q, c, s):
             maxA = a
     return maxA
 
-def state_action_sim(b,*args):
+def state_action_sim(s, ship_sizes, *args):
     """Compute the gradient of the loss with respect to theta."""
-    r = rewards(b._boardHitMiss)
-    counts = simulated_counts(b)
-    idx_hits = np.where(b._boardHitMiss == 2)
+    r = rewards(s)
+    counts = simulated_counts(s, ship_sizes)
+    idx_hits = np.where(s == 2)
     for i in range(np.size(idx_hits[0])): #don't want to pick a location we've already hit
         counts[idx_hits[0][i],idx_hits[1][i]] = 0
     Ts = transition_probability(counts)
@@ -35,14 +35,14 @@ def state_action_sim(b,*args):
         i,j = np.unravel_index(Ts.argmax(), Ts.shape)
         a = np.array([i,j])
     p = Ts[a[0],a[1]] #probabiliy of success
-    b._boardHitMiss[a[0],a[1]] = 1 + np.random.binomial(n, p) #hit = 2, miss = 1, not guessed = 0
-    return b,r
+    s[a[0],a[1]] = 1 + np.random.binomial(n, p) #hit = 2, miss = 1, not guessed = 0
+    return s, r
 
-def rollout(b,gamma,d):
-    if (d <= 0) | (np.sum(b._boardHitMiss == 0) == 0):
+def rollout(s, gamma, d):
+    if (d <= 0) | (np.sum(s == 0) == 0):
         return 0
-    b,r = state_action_sim(b)
-    return r + gamma*rollout(b,gamma,d - 1)
+    s, r = state_action_sim(s)
+    return r + gamma*rollout(s, gamma, d - 1)
 
 def rewards(s):
     total_guessed = np.sum(s > 0)
@@ -51,14 +51,14 @@ def rewards(s):
     else:
         return np.sum(s == 2)/np.sum(s > 0) #number of hits over number of guesses
 
-def simulated_counts(b):
-    counts = np.zeros(np.shape(b._boardHitMiss))
+def simulated_counts(s, ship_sizes):
+    counts = np.zeros(np.shape(s))
     num_samples = 10000
     for i in range(num_samples):
-        available_space = (b._boardHitMiss != 1)
-        for ship_length, num_ships in b.ship_sizes.items(): #will work better if ship sizes is organized in order of largests ship first
+        available_space = (s != 1)
+        for ship_length, num_ships in ship_sizes.items(): #will work better if ship sizes is organized in order of largests ship first
             for ship in range(num_ships):
-                x1,y1,x2,y2 = try_to_place(ship_length,available_space,b)
+                x1,y1,x2,y2 = try_to_place(ship_length,available_space, s)
                 if (x1 != x2) | (y1 != y2):
                     counts[y1:y2+1,x1:x2+1] += 1 #upper leftmost corner is x,y = 0,0
                     available_space[y1:y2+1,x1:x2+1] = 0
@@ -68,8 +68,8 @@ def simulated_counts(b):
 # need a way to check that we don't try to add a ship if it means that tot_num_ship_points < hits + current_ship_length
 # handle edge case where nothing is placed and probability is all Nan or zero
 # fix probability s.t. no divide by zero
-def try_to_place(ship_length,available_space,b):
-    hits_left = ((b._boardHitMiss == 2) & (available_space == 1))
+def try_to_place(ship_length, available_space, s):
+    hits_left = ((s == 2) & (available_space == 1))
     idx_hits_left = np.where(hits_left == 1)
     idx_available_space = np.where(available_space == 1)
 
@@ -90,17 +90,17 @@ def try_to_place(ship_length,available_space,b):
         if orientation == 0: #go right 
             x2 = x1 + ship_length - 1
             y2 = y1
-            if (x2 < b.board_width) & (np.sum(available_space[y1:y2+1,x1:x2+1]) == ship_length): 
+            if (x2 < s.shape[1]) & (np.sum(available_space[y1:y2+1,x1:x2+1]) == ship_length): 
                 return x1,y1,x2,y2
         elif orientation == 1: #go down
             x2 = x1 
             y2 = y1 + ship_length - 1
-            if (y2 < b.board_height) & (np.sum(available_space[y1:y2+1,x1:x2+1]) == ship_length): 
+            if (y2 < s.shape[0]) & (np.sum(available_space[y1:y2+1,x1:x2+1]) == ship_length): 
                 return x1,y1,x2,y2
         elif orientation == 2: #go left 
             x2 = x1 - (ship_length - 1)
             y2 = y1 
-            if (x2 >= 0) & (y2 < b.board_height) & (np.sum(available_space[y1:y2+1,x2:x1+1]) == ship_length): 
+            if (x2 >= 0) & (y2 < s.shape[0]) & (np.sum(available_space[y1:y2+1,x2:x1+1]) == ship_length): 
                 return x2,y1,x1,y2
         else: #go up
             x2 = x1 
