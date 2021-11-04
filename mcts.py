efdd07670f8e64633fcd0c35675e99a806e7d36d
Anna Boonyanit <annaboon@stanford.edu>
2021-11-03 21:15:30 -0700

    Add mcts code

--- mcts.py
+++ mcts.py
@@ -23,8 +23,40 @@ def main():
         for j in range(board_height):
             A.append((i,j))
     r = simulate.rollout(b,gamma,d)
-    
+    s = b._boardHitMiss
+    MCTS(s, board_width, board_height, c=1, d=10, discount_factor=0.95, k_max=10)
 
+def MCTS(s, board_width, board_height, c, d, discount_factor, k_max):
+    A = []
+    for i in range(board_width):
+        for j in range(board_height):
+            A.append((i,j))
+    Q = {}
+    N = {}
+    for k in range(k_max):
+        # Q = np.zeros((board_width*board_height, board_width*board_height)) #why are these dimensions? 3 **, 
+        # N = np.zeros((board_width*board_height, board_width*board_height))
+        simulate(d, s, A, discount_factor, c, Q, N)
+    return Q
+
+# Add function to map State (number) to the board
+    
+def simulate(d, s, A, discount_factor, c, Q, N):
+    if d <= 0:
+        return 0
+    if (s, A[0]) not in N.keys():
+        for a in A:
+            N[(s, a)] = 0
+            Q[(s, a)] = 0
+        return simulate.rollout(s, discount_factor, d) #can it be s? s = b._boardHitMiss
+    # Q = np.zeros((num_states, num_actions))
+    # N = np.zeros((num_states, num_actions))
+    a = simulate.explore(A, N, Q, c, s)
+    s_prime, r = simulate.state_action_sim(s, a)
+    q = r + discount_factor * simulate(d - 1, s_prime, A, discount_factor, c, Q, N)
+    N[(s, a)] += 1
+    Q[(s, a)] += (q - Q[(s, a)]) / N[(s, a)]
+    return q
 
 if __name__ == '__main__':
     main()
